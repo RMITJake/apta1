2b4a650bb4e08425e470fa25c5ca6c82a7940e11
RMITJake <s3905550@student.rmit.edu.au>
2023-06-02 00:15:40 +1000

    Commented out code and starting build

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 #include <string>
 
 #include "Types.h"
-#include "Node.h"
-#include "NodeList.h"
-#include "PathSolver.h"
+// #include "Node.h"
+// #include "NodeList.h"
+// #include "PathSolver.h"
 
 // Helper test functions
 void testNode();
@@ -17,7 +17,7 @@ void readEnvStdin(Env env);
 
 // Print out a Environment to standard output with path.
 // To be implemented for Milestone 3
-void printEnvStdout(Env env, NodeList* solution);
+// void printEnvStdout(Env env, NodeList* solution);
 
 
 int main(int argc, char** argv){
@@ -32,77 +32,89 @@ int main(int argc, char** argv){
 
     // Load Environment 
     Env env;
+    std::cout << "Loaded env" << std::endl;
     readEnvStdin(env);
-    
+
     // Solve using forwardSearch
     // THIS WILL ONLY WORK IF YOU'VE FINISHED MILESTONE 2
-    PathSolver* pathSolver = new PathSolver();
-    pathSolver->forwardSearch(env);
+    // PathSolver* pathSolver = new PathSolver();
+    // pathSolver->forwardSearch(env);
 
-    NodeList* exploredPositions = nullptr;
-    exploredPositions = pathSolver->getNodesExplored();
+    // NodeList* exploredPositions = nullptr;
+    // exploredPositions = pathSolver->getNodesExplored();
 
-    // Get the path
-    // THIS WILL ONLY WORK IF YOU'VE FINISHED MILESTONE 3
-    NodeList* solution = pathSolver->getPath(env);
+    // // Get the path
+    // // THIS WILL ONLY WORK IF YOU'VE FINISHED MILESTONE 3
+    // NodeList* solution = pathSolver->getPath(env);
 
-    printEnvStdout(env, solution);
+    // printEnvStdout(env, solution);
 
-    delete pathSolver;
-    delete exploredPositions;
-    delete solution;
+    // delete pathSolver;
+    // delete exploredPositions;
+    // delete solution;
 
 }
 
 void readEnvStdin(Env env){
-    //TODO 
+    std::cout << "###########################" << std::endl;
+    std::cout << "# Inside readEncStdin #####" << std::endl;
+    std::cout << "###########################" << std::endl;
+    std::cout << env;
+
+    for (int row = 0; row < ENV_DIM; row++)
+    {
+        for (int col = 0; col < ENV_DIM; col++)
+        {
+
+        }
+    }
 }
 
-void printEnvStdout(Env env, NodeList* solution) {
-    //TODO
-}
+// void printEnvStdout(Env env, NodeList* solution) {
+//     //TODO
+// }
 
 void testNode() {
     std::cout << "TESTING Node" << std::endl;
 
     // Make a Node and print out the contents
-    Node* node = new Node(1, 1, 2);
-    std::cout << node->getRow() << ",";
-    std::cout << node->getCol() << ",";
-    std::cout << node->getDistanceTraveled() << std::endl;
-    delete node;
+    // Node* node = new Node(1, 1, 2);
+    // std::cout << node->getRow() << ",";
+    // std::cout << node->getCol() << ",";
+    // std::cout << node->getDistanceTraveled() << std::endl;
+    // delete node;
 
     // Change Node and print again
-    node = new Node(4, 2, 3);
-    std::cout << node->getRow() << ",";
-    std::cout << node->getCol() << ",";
-    std::cout << node->getDistanceTraveled() << std::endl;
-    delete node;
+    // node = new Node(4, 2, 3);
+    // std::cout << node->getRow() << ",";
+    // std::cout << node->getCol() << ",";
+    // std::cout << node->getDistanceTraveled() << std::endl;
+    // delete node;
 }
 
 void testNodeList() {
     std::cout << "TESTING NodeList" << std::endl;
 
     // Make a simple NodeList, should be empty size
-    NodeList* nodeList = new NodeList();
-    std::cout << "NodeList size: " << nodeList->getLength() << std::endl;
+    // NodeList* nodeList = new NodeList();
+    // std::cout << "NodeList size: " << nodeList->getLength() << std::endl;
 
     // Add a Node to the NodeList, print size
-    Node* b1 = new Node(1, 1, 1);
-    nodeList->addElement(b1);
-    std::cout << "NodeList size: " << nodeList->getLength() << std::endl;
+    // Node* b1 = new Node(1, 1, 1);
+    // nodeList->addElement(b1);
+    // std::cout << "NodeList size: " << nodeList->getLength() << std::endl;
 
     // Add second Nodetest
-    Node* b2 = new Node(0, 0, 1);
-    nodeList->addElement(b2);
-    std::cout << "NodeList size: " << nodeList->getLength() << std::endl;
+    // Node* b2 = new Node(0, 0, 1);
+    // nodeList->addElement(b2);
+    // std::cout << "NodeList size: " << nodeList->getLength() << std::endl;
 
     // Test Get-ith - should be 0,0,1
-    Node* getB = nodeList->getNode(1);
-    std::cout << getB->getRow() << ",";
-    std::cout << getB->getCol() << ",";
-    std::cout << getB->getDistanceTraveled() << std::endl;
+    // Node* getB = nodeList->getNode(1);
+    // std::cout << getB->getRow() << ",";
+    // std::cout << getB->getCol() << ",";
+    // std::cout << getB->getDistanceTraveled() << std::endl;
 
     // Print out the NodeList
-    std::cout << "PRINTING OUT A NODELIST IS AN EXERCISE FOR YOU TO DO" << std::endl;
+    // std::cout << "PRINTING OUT A NODELIST IS AN EXERCISE FOR YOU TO DO" << std::endl;
 }
